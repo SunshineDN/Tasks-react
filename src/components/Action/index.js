c30bc724fd57201f76eab03168228a80a2e8273b
Aluno <aluno@naverecife.local>
2023-03-02 20:22:57 -0300

    Adicionado botões e props

--- src/components/Action/index.js
+++ src/components/Action/index.js
@@ -6,19 +6,25 @@ export default function Action(){
     const [id, setId] = useState(1)
     const [title, setTitle] = useState('');
     const [desc, setDesc] = useState('');
+    const [chooseDate, setChooseDate] = useState("");
     const [tasks, setTasks] = useState([]);
 
     function clear() {
         setTitle("");
         setDesc("");
+        setChooseDate("");
     }
 
     function handleAdd() {
         if (title === "" || desc === ""){
             return
         }
-        let date = new Date().toLocaleString("pt-BR")
-        setTasks([{id:id, title: title, description: desc, date: date.split(",").join("")}, ...tasks]);
+
+        let date = new Date().toLocaleString("pt-BR").split(",").join("");
+        let newDate = chooseDate.split("T").join(" ");
+
+        console.log(Math.abs(date[0] - newDate[0]))
+        setTasks([{id:id, title: title, description: desc, date: date, chooseDate: newDate}, ...tasks]);
         setId(e => e += 1)
         clear();
     }
@@ -33,9 +39,10 @@ export default function Action(){
             <div>
                 <input className="title" type="text" value={title} placeholder="Título" onChange={e => setTitle(e.target.value)} />
                 <input className="desc" type="text" value={desc} placeholder="Descrição" onChange={e => setDesc(e.target.value)} />
-                <button onClick={handleAdd}>Adicionar</button>
+                <input className="date" type="datetime-local" value={chooseDate} placeholder="Informe a data limite" onChange={e => setChooseDate(e.target.value)} />
+                <button className="addBtn" onClick={handleAdd}>Adicionar</button>
             </div>
-            {tasks.map( t => <div key={t.id}><Tasks title={t.title} desc={t.description} date={t.date}></Tasks><button onClick={e => handleClearTask(t)}>X</button></div>)}
+            {tasks.map( t => <div key={t.id}><Tasks title={t.title} desc={t.description} date={t.date} chooseDate={t.chooseDate}></Tasks><button className="removeBtn" onClick={e => handleClearTask(t)}>X</button></div>)}
         </div>
     )
 }
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,9 +1,12 @@
-export default function Tasks({title, desc, date}){
+import "./style.css"
+
+export default function Tasks({title, desc, date, chooseDate}){    
     return(
         <div>
             <h1>{title}</h1>
             <h2>{desc}</h2>
-            <h2>{date}</h2>
+            <h3>{date}</h3>
+            <h3>{chooseDate}</h3>
             <br />
         </div>
     )
