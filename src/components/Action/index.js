210e5ef8beb192db0a3e116efa68aa5124705abd
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-03 12:20:51 -0300

    Adicionado funções aos inputs e adicionado estilizações

--- src/components/Action/index.js
+++ src/components/Action/index.js
@@ -9,22 +9,30 @@ export default function Action() {
     const [chooseDate, setChooseDate] = useState("");
     const [tasks, setTasks] = useState([]);
 
+    const [styleTransparent, setStyleTransparent] = useState({color: "transparent"})
+
     function clear() {
         setTitle("");
         setDesc("");
         setChooseDate("");
+        setStyleTransparent({color: "transparent"});
     }
 
     function handleAdd() {
-        if (title === "" || desc === "") {
+        if (title === "" || desc === "" || chooseDate === "") {
             return
         }
-
+        
         let date = new Date().toLocaleString("pt-BR").split(",").join("");
         let newDate = chooseDate.split("T").join(" ");
 
+        const r = Math.floor(Math.random() * 255 + 1);
+        const g = Math.floor(Math.random() * 255 + 1);
+        const b = Math.floor(Math.random() * 255 + 1);
+        const color = `rgba(${r}, ${g}, ${b}, .5)`
+
         console.log(Math.abs(date[0] - newDate[0]))
-        setTasks([{ id: id, title: title, description: desc, date: date, chooseDate: newDate }, ...tasks]);
+        setTasks([{ id: id, title: title, description: desc, date: date, chooseDate: chooseDate.split("T").join(" "), color: color }, ...tasks]);
         setId(e => e += 1)
         clear();
     }
@@ -33,29 +41,37 @@ export default function Action() {
         setTasks(tasks.filter(t => t !== task))
     }
 
+    function handleChangeColor(value){
+        if (value !== ""){
+            setStyleTransparent({color: "white"});
+        } else {
+            setStyleTransparent({color: "transparent"});
+        }
+    }
+
     return (
         <div>
             <div className="container">
                 <h1>DoTask</h1>
-                <div className="wrapper">
+                <form className="wrapper">
                     <div className="input-box">
-                        <input className="title" type="text" value={title} onChange={e => setTitle(e.target.value)} />
+                        <input className="title" type="text" value={title} onChange={e => setTitle(e.target.value)} required />
                         <label>Título</label>
                     </div>
 
                     <div className="input-box">
-                        <input className="desc" type="text" value={desc} onChange={e => setDesc(e.target.value)} />
+                        <input className="desc" type="text" value={desc} onChange={e => setDesc(e.target.value)} required />
                         <label>Descrição</label>
                     </div>
-
                     <div className="input-box">
-                        <input className="date" type="datetime-local" value={chooseDate} onChange={e => setChooseDate(e.target.value)} />
-                        <label>Data</label>
+                        <input className="date" style={styleTransparent} type="datetime-local" value={chooseDate} onChange={e => setChooseDate(e.target.value, handleChangeColor(e.target.value))} required />
+                        <label>Data limite</label>
                     </div>
-                    <button className="addBtn" onClick={handleAdd}>Adicionar</button>
-                </div>
+
+                    <input type="submit" className="addBtn" onClick={handleAdd} value="Adicionar" />
+                </form>
             </div>
-            {tasks.map(t => <div key={t.id}><Tasks title={t.title} desc={t.description} date={t.date} chooseDate={t.chooseDate}></Tasks><button className="removeBtn" onClick={e => handleClearTask(t)}>X</button></div>)}
+            {tasks.map(t => <div key={t.id}><Tasks title={t.title} desc={t.description} randomColor={t.color} date={t.date} chooseDate={t.chooseDate}></Tasks><button className="removeBtn" onClick={e => handleClearTask(t)}>X</button></div>)}
         </div>
     )
 }
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,13 +1,31 @@
 import "./style.css"
 
-export default function Tasks({title, desc, date, chooseDate}){    
+export default function Tasks({title, desc, date, chooseDate, randomColor}){    
     return(
-        <div className="tasks-container">
-            <h1>{title}</h1>
-            <h2>{desc}</h2>
-            <h3>{date}</h3>
-            <h3>{chooseDate}</h3>
-            <br />
+        <div className="tasks-container" style={{"backgroundColor": randomColor}}>
+            <div className="task-wrapper">
+                <div className="text-box">
+                    <h1>Título:</h1>
+                    <p>{title}</p>
+                </div>
+                <div className="text-box">
+                    <h1>Descrição:</h1>
+                    <p>{desc}</p>
+                </div>
+                <div className="text-box">
+                    <h1>Data limite:</h1>
+                    <p>{chooseDate}</p>
+                </div>
+                <div className="date-wrapper">
+                    <h3>Tempo restante:</h3>
+                    <div className="date-box">
+                        <p>2</p><h4>Dias</h4>
+                        <p>2</p><h4>Horas</h4>
+                        <p>2</p><h4>Minutos</h4>
+                        <p>2</p><h4>Segundos</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
